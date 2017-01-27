e2714813d59b545559305c58e41a7085682b32df
matt kersner <mattkersner@gmail.com>
2017-01-26 20:10:47 -0500

    Add ability to delete users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../server/models/index');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+  models.User.findAll({}).then(function(users) {
+    res.render('users/index', {
+      title: 'fazbook',
+      users: users
+    });
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new', {
+    title: 'new'
+  });
+});
+
+//This uses the Sequelize model create method to add a user record to the
+// database. The object you provide tells it what attributes to set.
+// After the user record is committed to the db, the then callback
+// redirects us to the users index page.
+router.post('/', function(req, res, next) {
+  models.User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  }).then(function() {
+    res.redirect('/users')
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  models.User.destroy({
+    where: { id: req.params.id }
+  }).then(function(user) {
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
